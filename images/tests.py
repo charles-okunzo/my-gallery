2f16b7c1ff3c55b12123d8f08c6b8f3e65eab33a
charles okunzo <okunzopeter01@gmail.com>
2022-05-28 19:13:05 +0300

    feature --add testcase for location

--- images/tests.py
+++ images/tests.py
@@ -1,5 +1,16 @@
 from django.test import TestCase
+from .models import *
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+
+  def setUp(self):
+    self.new_location = Location(name = 'Nairobi')
+
+  def test_save_location(self):
+    self.new_location.save_location()
+    locations = Location.objects.all()
+    self.assertTrue(len(locations)>0)
 
 
