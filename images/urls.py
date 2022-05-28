c2fc4d15ec66d60b7e034351c6b346da0c3f11fe
charles okunzo <okunzopeter01@gmail.com>
2022-05-28 15:24:23 +0300

    FEATURE --CONFIGURE IMAGE FIELD SETTINGS

--- images/urls.py
+++ images/urls.py
@@ -1,9 +1,13 @@
 from django import views
 from django.urls import path, re_path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
   path('', views.home, name= 'home')
 ]
+
+urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
