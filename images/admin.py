9b25cd565edb4f6489f0316da904466dc83dc849
charles okunzo <okunzopeter01@gmail.com>
2022-05-28 17:36:21 +0300

    feature --register classes to admin

--- images/admin.py
+++ images/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from images.models import Category, Image, Location
+from .views import *
 # Register your models here.
+
+
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
