9c5e65006f41da6cd0ddc544430aefe909065a3d
charles okunzo <okunzopeter01@gmail.com>
2022-05-29 16:31:40 +0300

    style --add recent images view

--- images/views.py
+++ images/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+from images.models import Image
+
 # Create your views here.
 
 
 def home(request):
 
+  all_images = Image.objects.order_by('-created_at')
+  all_categories = Image.objects.order_by('category')
 
 
-  return render(request, 'index.html')
+
+  return render(request, 'index.html', {'all_images':all_images, 'all_category':all_categories})
