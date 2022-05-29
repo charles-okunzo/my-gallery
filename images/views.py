cd46e97581261b5ac70260bbef28b80d16bdff77
charles okunzo <okunzopeter01@gmail.com>
2022-05-29 17:56:58 +0300

    style --add teplates for categories and locations

--- images/views.py
+++ images/views.py
@@ -8,8 +8,9 @@ from images.models import Image
 def home(request):
 
   all_images = Image.objects.order_by('-created_at')
-  all_categories = Image.objects.order_by('category')
+  all_categories = Image.objects.order_by('-category')
+  all_locations = Image.objects.order_by('location')
 
 
 
-  return render(request, 'index.html', {'all_images':all_images, 'all_category':all_categories})
+  return render(request, 'index.html', {'all_images':all_images, 'all_categories':all_categories, 'all_locations': all_locations})
