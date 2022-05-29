00c0bf159b25396713b100d5d197f0314a6962ed
charles okunzo <okunzopeter01@gmail.com>
2022-05-29 18:21:11 +0300

    faeture --add search view function

--- images/views.py
+++ images/views.py
@@ -14,3 +14,15 @@ def home(request):
 
 
   return render(request, 'index.html', {'all_images':all_images, 'all_categories':all_categories, 'all_locations': all_locations})
+
+
+def search_by_category(request):
+  if 'image_by_cat' in request.GET and request.GET['image_by_cat']:
+    search_phrase = request.GET.get('image_by_cat')
+    searched_images = Image.search_image(search_phrase)
+    message = f'{search_phrase}'
+
+  else:
+    message = 'You haven\'t searches for any images' 
+
+  return render(request, 'search_category.html', {'searched_images': searched_images, 'message': message})
